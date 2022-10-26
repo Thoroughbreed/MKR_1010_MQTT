b71ffd64c06790431a1cf33e33265b6cf30ccfb1
Jan Andreasen <jan@tved.it>
2022-10-26 13:38:51 +0200

    MQTT PUB SUB works

--- MKR_1010_MQTT.ino
+++ MKR_1010_MQTT.ino
@@ -1,66 +1,6 @@
-#include <SPI.h>                  // SPI
-#include <WiFiNINA.h>             // Wireless
-#include <utility/wifi_drv.h>     // Builtin RGB
-#include <DHT.h>                  // DHT
-#include <Wire.h>                 // I2C
-#include <Adafruit_GFX.h>         // OLED
-#include <Adafruit_SSD1306.h>     // OLED
-#include "Adafruit_MQTT.h"        // MQTT
-#include "Adafruit_MQTT_Client.h" // MQTT
+#include "shared.h"
 
-/************************* WiFi Access Point *********************************/
-#define WLAN_SSID       "SibirienAP"
-#define WLAN_PASS       "Siberia51244"
-
-/************************* Brooker Setup *********************************/
-#define AIO_SERVER      "hackerman.cloud.shiftr.io"
-#define AIO_SERVERPORT  1883                   // use 8883 for SSL
-#define AIO_USERNAME    "hackerman"
-#define AIO_KEY         "NKV20rskFJWP8b7A"
-
-/************************* Var & const *********************************/
-String messageToDisplay;    // Message for OLED
-long delayOLED;
-long delayPub;
-long delayClimate;
-WiFiClient client;          // Wireless client
-//WiFiClientSecure client;  // SSL
-uint32_t t = 0;             // Temp from DHT11
-uint32_t h = 0;             // Humidity from DHT11
-
-/************************* Devices/classes *********************************/
-// DHT
-#define DHT_PIN 6
-#define DHTTYPE DHT11
-DHT dht(DHT_PIN, DHTTYPE);
-
-// OLED
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define OLED_RESET -1
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-// MQTT
-Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
-/****************************** Feeds ***************************************/
-// PUB
-Adafruit_MQTT_Publish temp = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/climate/temperature");
-Adafruit_MQTT_Publish humid = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/climate/humidity");
-
-// SUB
-Adafruit_MQTT_Subscribe text = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/text/oled");
-
-/************************* Func prototyping *********************************/
-void MQTT_connect();
-void mqttSub();
-void mqttPub(int interval);
-void mqttPubAll(uint32_t value, Adafruit_MQTT_Publish topic);
-void getClimate(int interval);
-void initDisplay();
-void initRGB();
-void printOLED(int x, int y, String text, int textSize = 1);
-void updateOLED(int interval);
-void flashWhite(int interval);
+#pragma region Initialization
 
 void initRGB()
 {
@@ -78,22 +18,8 @@ void initDisplay()
 	}
 }
 
-void setup()
+void initWireless();
 {
-  Serial.begin(9600);
-  dht.begin();
-  initDisplay();
-  initRGB();
-  display.clearDisplay();
-  delay(10);
-
-  Serial.println(F("Adafruit MQTT demo"));
-
-  // Connect to WiFi access point.
-  Serial.println(); Serial.println();
-  ledRed();
-  Serial.print("Connecting to ");
-  Serial.println(WLAN_SSID);
   printOLED(0, 0, "Connecting to:", 1);
   printOLED(0, 10, WLAN_SSID, 2);
   display.display();
@@ -102,21 +28,31 @@ void setup()
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
-    Serial.print(".");
     display.setCursor(i, 20);
     display.write(".");
     display.display();
     i++;
   }
-  Serial.println();
   display.clearDisplay();
   printOLED(0, 0, "WiFi connected!", 2);
-  Serial.println("WiFi connected");
-  Serial.println("IP address: "); Serial.println(WiFi.localIP());
   delay(1000);
-  mqtt.subscribe(&text);
 }
 
+void setup()
+{
+  initRGB();
+  ledRed();
+  Serial.begin(9600);
+  dht.begin();          
+  initDisplay();
+  display.clearDisplay();
+  delay(10);
+  initWireless();
+  mqtt.subscribe(&text);
+}
+#pragma endregion
+
+#pragma region OLED
 void printOLED(int x, int y, String text, int textSize)
 {
   display.setTextSize(textSize);
@@ -135,64 +71,9 @@ void updateOLED(int interval)
     display.display();
   }
 }
+#pragma endregion
 
-void mqttSub()
-{
-  Adafruit_MQTT_Subscribe *subscription;
-  while ((subscription = mqtt.readSubscription())) {
-    if (subscription == &text)
-    {
-      Serial.print(F("Got: "));
-      Serial.println((char *)text.lastread);
-      messageToDisplay = (char *)text.lastread;
-      Serial.println(messageToDisplay);
-      flashWhite(75);
-      delayClimate = millis();
-    }
-  }  
-}
-
-void mqttPub(int interval)
-{
-  if ((millis() - delayPub) > interval)
-  {
-    mqttPubAll(t, temp);
-    mqttPubAll(h, humid);
-  }
-}
-
-void mqttPubAll(uint32_t value, Adafruit_MQTT_Publish topic)
-{
-  delayPub = millis();
-  if (! topic.publish(value))
-  {
-    Serial.println(F("Failed"));
-  }
-}
-
-void getClimate(int interval)
-{
-  if ((millis() - delayClimate) > interval)
-  {
-    delayClimate = millis();
-    t = dht.readTemperature();
-    h = dht.readHumidity();
-    messageToDisplay = "";
-    messageToDisplay += t;
-    messageToDisplay += "C  ";
-    messageToDisplay += h;
-    messageToDisplay += "%rH";
-  }
-}
-
-void loop() {
-  MQTT_connect();
-  mqttSub();
-  getClimate(5000);
-  mqttPub(5000);
-  updateOLED(500);
-}
-
+#pragma region MQTT pub/sub
 void MQTT_connect()
 {
   int8_t ret;
@@ -231,6 +112,59 @@ void MQTT_connect()
   delay(1234);
 }
 
+void mqttSub()
+{
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription())) {
+    if (subscription == &text)
+    {
+      Serial.print(F("Got: "));
+      Serial.println((char *)text.lastread);
+      messageToDisplay = (char *)text.lastread;
+      Serial.println(messageToDisplay);
+      flashWhite(75);
+      delayClimate = millis();
+    }
+  }  
+}
+
+void mqttPub(int interval)
+{
+  if ((millis() - delayPub) > interval)
+  {
+    mqttPubAll(t, temp);
+    mqttPubAll(h, humid);
+  }
+}
+
+void mqttPubAll(uint32_t value, Adafruit_MQTT_Publish topic)
+{
+  delayPub = millis();
+  if (! topic.publish(value))
+  {
+    Serial.println(F("Failed"));
+  }
+}
+#pragma endregion
+
+#pragma region Misc functions
+void getClimate(int interval)
+{
+  if ((millis() - delayClimate) > interval)
+  {
+    delayClimate = millis();
+    t = dht.readTemperature();
+    h = dht.readHumidity();
+    messageToDisplay = "";
+    messageToDisplay += t;
+    messageToDisplay += "C  ";
+    messageToDisplay += h;
+    messageToDisplay += "%rH";
+  }
+}
+#pragma endregion
+
+#pragma region LED Control
 void flashWhite(int interval)
 {
   WiFiDrv::digitalWrite(25, 1);   //GREEN
@@ -278,4 +212,13 @@ void ledBlue()
   WiFiDrv::digitalWrite(25, 0);   //GREEN
   WiFiDrv::digitalWrite(26, 0);   //RED
   WiFiDrv::digitalWrite(27, 1);   //BLUE
+}
+#pragma endregion
+
+void loop() {
+  MQTT_connect();
+  mqttSub();
+  getClimate(5000);
+  mqttPub(5000);
+  updateOLED(500);
 }
