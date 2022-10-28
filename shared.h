9060a41b6506bad76ce1987b784d2df7dfa493d4
Jan Andreasen <jan@tved.it>
2022-10-28 12:09:08 +0200

    HiveMQ doesn't really work

--- shared.h
+++ shared.h
@@ -9,17 +9,28 @@
 #include "Adafruit_MQTT_Client.h" // MQTT
 #include <ArduinoJson.h>          // JSON
 #include <Servo.h>                // Servo
+#include <NTPClient.h>            // NTP client
+#include <WiFiUdp.h>              // NTP client
 
 /************************* WiFi Access Point *********************************/
 #define WLAN_SSID       "SibirienAP"
 #define WLAN_PASS       "Siberia51244"
 
+/************************* NTP cluent *********************************/
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
 /************************* Brooker Setup *********************************/
-#define AIO_SERVER      "mqtt3.thingspeak.com"
+#define AIO_SERVER      "00e34ed85c824b8c80012765b51ba787.s1.eu.hivemq.cloud"
 #define AIO_SERVERPORT  1883                   // use 8883 for SSL
-#define AIO_USERNAME    "Fw0mJzQqJBQ0JyQgACkwJig"
-#define AIO_CLIENTID    "Fw0mJzQqJBQ0JyQgACkwJig"
-#define AIO_KEY         "JnhZVIxT2gutEAGtCBLSEFMO"
+#define AIO_USERNAME    "ardui"
+#define AIO_CLIENTID    "MKR1010"
+#define AIO_KEY         "qiZqij-puxdut-4warju"
+// #define AIO_SERVER      "mqtt3.thingspeak.com"
+// #define AIO_SERVERPORT  1883                   // use 8883 for SSL
+// #define AIO_USERNAME    "Fw0mJzQqJBQ0JyQgACkwJig"
+// #define AIO_CLIENTID    "Fw0mJzQqJBQ0JyQgACkwJig"
+// #define AIO_KEY         "JnhZVIxT2gutEAGtCBLSEFMO"
 
 /************************* Servo *********************************/
 #define SERVO_PIN       9
@@ -30,6 +41,7 @@ String messageToDisplay;    // Message for OLED
 long delayOLED;
 long delayPub;
 long delayClimate;
+long delayTime;
 WiFiClient client;          // Wireless client
 //WiFiClientSecure client;  // SSL
 int servoPos = 0;           
@@ -51,6 +63,13 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 // MQTT
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_CLIENTID, AIO_USERNAME, AIO_KEY);
 /****************************** Feeds ***************************************/
+// // PUB
+// Adafruit_MQTT_Publish pub = Adafruit_MQTT_Publish(&mqtt, "hackerman/test");
+// // Adafruit_MQTT_Publish humid = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/climate/humidity");
+
+// // SUB
+// Adafruit_MQTT_Subscribe sub = Adafruit_MQTT_Subscribe(&mqtt, "hackerman/test");
+
 // PUB
 Adafruit_MQTT_Publish pub = Adafruit_MQTT_Publish(&mqtt, "channels/1910465/publish");
 // Adafruit_MQTT_Publish humid = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/climate/humidity");
@@ -65,11 +84,13 @@ void mqttPub(int interval);
 void mqttPubAll(uint32_t value, Adafruit_MQTT_Publish topic);
 void mqqtSubParse(const char*);
 void getClimate(int interval);
+void getTime();
 void runServo(int deg);
 void serialLog(const char*);
 void initServo();
 void initDisplay();
 void initWireless();
+void initDateTime();
 void initRGB();
 void printOLED(int x, int y, String text, int textSize = 1);
 void updateOLED(int interval);
