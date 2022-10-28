fce3340eb281f8574d75e8497ebc5150fddafee7
Jan Andreasen <jan@tved.it>
2022-10-28 12:10:19 +0200

    now with NTP

--- shared.h
+++ shared.h
@@ -9,10 +9,16 @@
 #include "Adafruit_MQTT_Client.h" // MQTT
 #include <ArduinoJson.h>          // JSON
 #include <Servo.h>                // Servo
+#include <NTPClient.h>            // NTP client
+#include <WiFiUdp.h>              // NTP client
 
 /************************* WiFi Access Point *********************************/
 #define WLAN_SSID       "SibirienAP"
 #define WLAN_PASS       "Siberia51244"
+
+/************************* NTP cluent *********************************/
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
 
 /************************* Brooker Setup *********************************/
 #define AIO_SERVER      "mqtt3.thingspeak.com"
@@ -30,6 +36,7 @@ String messageToDisplay;    // Message for OLED
 long delayOLED;
 long delayPub;
 long delayClimate;
+long delayTime;
 WiFiClient client;          // Wireless client
 //WiFiClientSecure client;  // SSL
 int servoPos = 0;           
@@ -51,6 +58,13 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 // MQTT
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_CLIENTID, AIO_USERNAME, AIO_KEY);
 /****************************** Feeds ***************************************/
+// // PUB
+// Adafruit_MQTT_Publish pub = Adafruit_MQTT_Publish(&mqtt, "hackerman/test");
+// // Adafruit_MQTT_Publish humid = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/climate/humidity");
+
+// // SUB
+// Adafruit_MQTT_Subscribe sub = Adafruit_MQTT_Subscribe(&mqtt, "hackerman/test");
+
 // PUB
 Adafruit_MQTT_Publish pub = Adafruit_MQTT_Publish(&mqtt, "channels/1910465/publish");
 // Adafruit_MQTT_Publish humid = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/climate/humidity");
@@ -65,11 +79,13 @@ void mqttPub(int interval);
 void mqttPubAll(uint32_t value, Adafruit_MQTT_Publish topic);
 void mqqtSubParse(const char*);
 void getClimate(int interval);
+void getTime();
 void runServo(int deg);
 void serialLog(const char*);
 void initServo();
 void initDisplay();
 void initWireless();
+void initDateTime();
 void initRGB();
 void printOLED(int x, int y, String text, int textSize = 1);
 void updateOLED(int interval);
